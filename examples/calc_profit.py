da02920f599c9dbddc71209bc0c3939d8cf8618a
WUYUAN CHEN <wuyuanchen123@gmail.com>
2021-03-25 21:02:21 -0700

    added a lib to calcuate APPT and wrote unit test for it

---
+++ examples/calc_profit.py
@@ -0,0 +1,19 @@
+import cbpro
+import os
+import sys, os.path
+root_dir = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(root_dir)
+from tenzin.crypto_lib.cbpro_weighted_api import CbproWeightedApi
+
+def main():
+    print("hello world")
+    public_client = cbpro.PublicClient()
+    api_url="https://api-public.sandbox.pro.coinbase.com"
+    auth_client = cbpro.AuthenticatedClient(os.environ["API_KEY"], os.environ["CLIENT_SECRET"], os.environ["PASSPHRASE"], api_url=api_url)
+
+    weighted_api = CbproWeightedApi(public_client, auth_client)
+    weighted_api.get_realized_gain()
+    weighted_api.get_appt()
+
+if __name__ == "__main__":
+    main()
